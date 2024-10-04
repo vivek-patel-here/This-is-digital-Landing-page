dcc5c2bc2e7ac197fb53645f4b1f85d148cc0ac3
vivek-patel-here <146237401+vivek-patel-here@users.noreply.github.com>
2024-10-04 21:05:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# This-is-digital-Landing-page
+# Project Title
+
+This is digital
+
+# Project description
+
+This Landing page is clone of an "this is digital " website..
+It is made up with HTML , CSS , JS ( GSAP Lib.) .
---
+++ app.js
@@ -0,0 +1,25 @@
+gsap.from(".mainpage",{
+    opacity:0,
+    duartion:0.2
+})
+
+gsap.from("#img1",{
+    delay:0.2,
+    duration:0.8,
+    opacity:0,
+    y:100
+})
+
+gsap.from("#img2",{
+    opacity:0,
+    x:50,
+    delay:0.3,
+    duration:0.8,
+})
+
+gsap.from("#img3",{
+    opacity:0,
+    delay:0.4,
+    duration:0.8,
+    y:-30
+})
